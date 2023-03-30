034cf3168f2e5f9748b029c0600dfc99a06256fc
fishe <donhatthanh1805@gmail.com>
2023-03-31 01:39:43 +0700

    Configure starting state.

--- initialize/productPageInit.py
+++ initialize/productPageInit.py
@@ -4,7 +4,4 @@ def init(parent, categories):
     for index, category in  enumerate(categories):
         attr_name = "page_" + category
         setattr(parent, attr_name, productPage.create_page(parent, category))
-
     parent.page_CPU.grid(row=0, column=1, sticky="nsew")
-    parent.page_GPU.grid(row=0, column=1, sticky="nsew")
-    parent.page_CPU.grid_forget()
